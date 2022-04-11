e6d765043d2460f4233329155b782d90860dc812
sambi0197 <leandro.irigoyen97@gmail.com>
2022-04-11 07:16:18 -0700

    vuelta3

--- exitfunc.c
+++ exitfunc.c
@@ -7,31 +7,29 @@
  */
 int exitFunc(config *cart)
 {
-		register int argCount, exitStatus;
+	register int argCount, exitStatus;
 
-		argCount = countArgs(cart->args);
-		if (argCount == 1)
+	argCount = countArgs(cart->args);
+	if (argCount == 1)
+	{
+		freeMembers(cart);
+
+		if (cart->errorStatus)
+			exit(cart->errorStatus);
+		exit(EXIT_SUCCESS);
+	}
+	else if (argCount > 1)
+	{
+		exitStatus = _atoi(cart->args[1]);
+		if (exitStatus == -1)
 		{
-			freeMembers(cart);
-
-			if (cart->errorStatus)
-			{
-				exit(cart->errorStatus);
-				exit(EXIT_SUCCESS);
-			}
-			else if (argCount > 1)
-			{
-				exitStatus = _atoi(cart->args[1]);
-				if (exitStatus == -1)
-				{
-					errno = EILLEGAL;
-					cart->errorStatus = 2;
-					errorHandler(cart);
-					return (0);
-				}
-				freeMembers(cart);
-				exit(exitStatus);
-			}
+			errno = EILLEGAL;
+			cart->errorStatus = 2;
+			errorHandler(cart);
+			return (0);
 		}
-		return (1);
+		freeMembers(cart);
+		exit(exitStatus);
+	}
+	return (1);
 }
